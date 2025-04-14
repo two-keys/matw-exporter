7fe510d8df798637846eb604065f7507efc58ffc
two-keys <dandys10@live.com>
2025-04-13 21:38:00 -0500

    Print spell practice

--- extractors/lib/spell.py
+++ extractors/lib/spell.py
@@ -2,6 +2,7 @@ from lib.shared import get_line_type, text_from_line, tab
 
 class Spell:
     name = ''
+    practice = ''
 
     def __init__(self, name_line):
         self.name = text_from_line(name_line)
@@ -14,9 +15,14 @@ class Spell:
             case 'spell name':
                 self.name = line_text
 
+            case 'spell practice':
+                self.practice = line_text
+
     def write_to_file(self, out):
         print(self.name)
         out.write(self.name.encode('utf8'))
-        out.write('\n'.encode('utf8'))
+        out.write(','.encode('utf8'))
         
-        print("%stest" %(tab(1)))
+        print("%s%s" %(tab(1), self.practice))
+        out.write(self.practice.encode('utf8'))
+        out.write('\n'.encode('utf8'))
