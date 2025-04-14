ba5c28582882aeb95dfa96222b6c9af7a88b754a
two-keys <dandys10@live.com>
2025-04-14 11:49:15 -0500

    Add spell miscllanea

--- extractors/lib/spell.py
+++ extractors/lib/spell.py
@@ -3,6 +3,10 @@ from lib.shared import get_line_type, text_from_line, tab
 class Spell:
     name = ''
     practice = ''
+    p_factor = ''
+    spell_cost = '0'
+    withstand = ''
+    rote_skills = ''
 
     def __init__(self, name_line):
         self.name = text_from_line(name_line)
@@ -18,6 +22,18 @@ class Spell:
             case 'spell practice':
                 self.practice = line_text
 
+            case 'spell primary factor':
+                self.p_factor = line_text
+
+            case 'spell cost':
+                self.spell_cost = line_text
+
+            case 'spell withstand':
+                self.withstand = line_text
+
+            case 'spell rote skills':
+                self.rote_skills = line_text
+
     def write_to_file(self, out):
         print(self.name)
         out.write(self.name.encode('utf8'))
@@ -25,4 +41,21 @@ class Spell:
         
         print("%s%s" %(tab(1), self.practice))
         out.write(self.practice.encode('utf8'))
+        out.write(','.encode('utf8'))
+
+        print("%s%s" %(tab(1), self.p_factor))
+        out.write(self.p_factor.encode('utf8'))
+        out.write(','.encode('utf8'))
+
+        print("%s%s" %(tab(1), self.spell_cost))
+        out.write(self.spell_cost.encode('utf8'))
+        out.write(','.encode('utf8'))
+
+        print("%s%s" %(tab(1), self.withstand))
+        out.write(self.withstand.encode('utf8'))
+        out.write(','.encode('utf8'))
+
+        print("%s%s" %(tab(1), self.rote_skills))
+        out.write(self.rote_skills.encode('utf8'))
+
         out.write('\n'.encode('utf8'))
